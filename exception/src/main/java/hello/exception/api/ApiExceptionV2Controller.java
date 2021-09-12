a74cf43b0f42ccfa8a047f7ed7f9a8836ce147c3
장유진/전시개발팀 <yujin.jang@musinsa.com>
2021-09-12 22:19:56 +0900

    09. API 예외 처리 꾸역꾸역

--- exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -1,22 +1,24 @@
 package hello.exception.api;
 
-import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
+import hello.exception.exhandler.ErrorResult;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @RestController
-public class ApiExceptionController {
+public class ApiExceptionV2Controller {
 
-    @GetMapping("/api/members/{id}")
+    @GetMapping("/api2/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
 
         if ("ex".equals(id)) {
@@ -31,34 +33,14 @@ public class ApiExceptionController {
             throw new UserException("사용자 오류");
         }
 
-
         return new MemberDto(id, "hello" + id);
-    }
-
-    @GetMapping("/api/response-status-ex1")
-    public String responseStatusEx1() {
-        throw new BadRequestException();
-    }
-
-
-    @GetMapping("/api/response-status-ex2")
-    public String responseStatusEx2() {
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad",
-            new IllegalArgumentException());
-    }
-
-    @GetMapping("/api/default-handler-ex")
-    public String defaultException(@RequestParam Integer data) {
-        return "ok";
     }
 
     @Data
     @AllArgsConstructor
     static class MemberDto {
-
         private String memberId;
         private String name;
     }
-
 
 }
